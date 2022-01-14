82ed471e723d64100866a417149f3cc179b88e05
Jerrett Davis <mxjerrett@gmail.com>
2022-01-13 19:30:36 -0600

    Removed internal docs folder for the time being

---
+++ src/FolderProcessor.Models/Processing/Configuration/WorkingFileSettings.cs
@@ -0,0 +1,9 @@
+namespace FolderProcessor.Models.Processing.Configuration;
+
+public class WorkingFileSettings
+{
+    /// <summary>
+    /// The folder to copy working files to
+    /// </summary>
+    public string Folder { get; set; }
+}
